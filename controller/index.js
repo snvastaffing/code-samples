e520008d81358451e0926f32fe655a6afc5dae01
Sabastain Wakoyi <sabastainwakoyi01@gmail.com>
2022-12-22 13:28:18 -0600

    resume

--- controller/index.js
+++ controller/index.js
@@ -1,4 +1,5 @@
-// this is a simple js file 
+/Users/jevian/IdeaProjects/resume-parsing-api/updatedSampleCode/ResumeParserModule/
+// this is a simple js file
 
 function doSomething(){
     return "Yes I am Doing";
